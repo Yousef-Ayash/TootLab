d99776f6ffb4ce915b6904ca60361912efb39e8d
Yousef-Ayash <yousef65ayash@gmail.com>
2025-04-23 17:00:08 +0300

    re style

--- resources/views/auth/role-select.blade.php
+++ resources/views/auth/role-select.blade.php
@@ -1,1 +1,15 @@
+@extends('layouts.app')
 
+@section('title', 'Choose Role')
+
+@section('content')
+    <div class="role-select">
+        <h1 class="page-title">Login As</h1>
+        <a href="{{ route('login', ['role' => 'doctor']) }}" class="btn btn--role btn--doctor">
+            Doctor
+        </a>
+        <a href="{{ route('login', ['role' => 'employee']) }}" class="btn btn--role btn--employee">
+            Employee / Admin
+        </a>
+    </div>
+@endsection
